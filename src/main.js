4ffbcb5a0676ab85fdb2e6f60a00370c6908eb93
김지수 <jsweetpotato37@gmail.com>
2022-06-03 15:20:15 +0900

    Detach the shader file

--- src/main.js
+++ src/main.js
@@ -1,32 +1,9 @@
 
 import { OrbitControls } from "./OrbitControls.js";
 
-const vertex = `
-  varying vec2 vUv;
-  void main()	{
-    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-    vUv = uv;
-  }
-`;
 
-const fragment = `
-  precision mediump float;
-  precision mediump int;
 
-  uniform float time;
 
-  varying vec2 vUv;
-
-  vec3 colorA = vec3(0.149,0.141,0.912);
-  vec3 colorB = vec3(1.000,0.833,0.224);
-
-  void main(){
-    vec3 color = vec3(0.0);
-    float pct = abs(sin(time*0.001));
-    color = mix(colorA, colorB, pct);
-    gl_FragColor =  vec4(color,1.0);
-  }
-`;
 
 class App {
   constructor() {
